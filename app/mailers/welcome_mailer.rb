85498454ab335e0f8835ee8133f0f5c014a4fda0
kushaGG <44055570+kushaGG@users.noreply.github.com>
2019-03-09 19:24:39 +0200

    Revert "email confirmation, gem pg"

--- app/mailers/welcome_mailer.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeMailer < ActionMailer::Base
-  default from: "Uzh-Nyam@gmail.com"
-
-   def welcome_email(user)
-    @user = user
-    mail(to: user.email, subject: 'Welcome, dear!', from:"Uzh-Nyam@gmail.com")
-  end
-end
--- lib/development_mail_interceptor.rb
+++
@@ -1,6 +0,0 @@
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.subject = "#{message.to} #{message.subject}"
-    message.to = @user.email
-  end
-end
