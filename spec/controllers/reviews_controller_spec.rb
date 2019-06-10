7cf9b7a0cc83806f4239ca09749a019648650e2a
sevcukd <sevcukd9@gmail.com>
2019-06-10 15:42:18 +0300

    fix tests controller

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe ReviewsController, type: :controller do
   let!(:user) { build(:user, email: "test@gmail.com") }
   login_user
-
-
+  
 it "should have a current_user" do
   expect(subject.current_user).to_not eq(nil)
 end
